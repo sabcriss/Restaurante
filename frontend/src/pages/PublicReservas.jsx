49083dbd6442a85587338f1b45e962f9d0febc2f
P0NT4S <marcosmyguel@gmail.com>
2026-06-16 19:36:09 -0300

    correção de erro por sensitive case

--- frontend/src/pages/PublicReservas.jsx
+++ frontend/src/pages/PublicReservas.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import GerenciadorReservas from '../components/GerenciadorReservas';
-import logoImg from '../assets/Logo.png';
+import logoImg from '../assets/logo.png';
 import '../App.css';
 
 /**
